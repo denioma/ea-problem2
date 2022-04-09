c7424de31e264247a67b2ad9247bfb8150da6661
Rodrigo Machado <29443508+denioma@users.noreply.github.com>
2022-04-09 13:59:19 +0000

    200 pontos: Aceite

--- main.cpp
+++ main.cpp
@@ -11,71 +11,76 @@ public:
     int recruiter;
     std::vector<int> recruited;
 
-    Member(int);
+    Member(int id) {
+        Member::id = id;
+        Member::recruiter = -1;
+    };
+
     ~Member() {};
-    bool operator<(Member);
 };
 
-Member::Member(int id) {
-    Member::id = id;
-    Member::recruiter = -1;
-}
-
-bool Member::operator<(Member other) {
-    return id < other.id;
-}
 
 void addRecruiters(std::vector<Member>& members) {
-    for (auto& member : members) {
+    for (const auto& member : members) {
         if (member.id == -1) continue;
-        for (auto& id : member.recruited) {
+        for (const auto& id : member.recruited) {
             members[id].recruiter = member.id;
         }
     }
 }
 
 std::vector<Member> members(100001, Member(-1));
-std::vector<bool> visited;
 std::vector<std::vector<std::vector<int>>> dp;
 
 int explore(int i, int guarded) {
-    if (members[i].recruited.empty()) { // Leaf
-        dp[i][1][1] = members[i].cost;
-        return guarded;
-    } else if (dp[i][guarded][0] != -1) { // If already cached
+    if (dp[i][guarded][0] != -1) { // If already cached
         return dp[i][guarded][0];
     }
 
-    int sum = 0;
+    if (members[i].recruited.empty()) { // Leaf
+        dp[i][0] = { 0, 0 };
+        dp[i][1] = { 1, members[i].cost };
+        return guarded;
+    }
+
+    int sum = guarded;
     for (const auto& id : members[i].recruited) {
         if (guarded == 0) { // Must select all child nodes for edge inclusion
             sum += explore(id, 1);
             // Adds label of all child nodes to cost
             dp[i][guarded][1] += dp[id][1][1];
         } else {
+            // Pick the minimum size solution
+            // If at a stalemate, pick the highest cost
             int tmp1 = explore(id, 0);
             int tmp2 = explore(id, 1);
-            if (tmp2 <= tmp1) {
-                dp[i][guarded][1] += members[id].cost;
+            if (tmp1 == tmp2) {
+                dp[i][guarded][1] += std::max(dp[id][0][1], dp[id][1][1]);
+            } else if (tmp1 < tmp2) {
+                dp[i][guarded][1] += dp[id][0][1];
+            } else {
+                dp[i][guarded][1] += dp[id][1][1];
             }
-            sum += std::min(explore(id, 0), explore(id, 1));
+            sum += std::min(tmp1, tmp2);
         }
     }
-    dp[i][guarded][0] = sum + guarded;
-    if (guarded) dp[i][guarded][1] += members[i].cost;
+
+    dp[i][guarded][0] = sum;
+    dp[i][guarded][1] += members[i].cost * guarded;
     return dp[i][guarded][0];
 }
 
 void investigate() {
-    visited = std::vector<bool>(members.size(), false);
+    // Create DP table
     std::vector<std::vector<int>> tmp(2, {-1, 0});
     dp = std::vector<std::vector<std::vector<int>>>(members.size(), tmp);
+
     int min = std::min(explore(0, 0), explore(0, 1));
     int max = 0;
-    if (dp[0][0][0] == min) {
-        if (dp[0][1][0] == min)
-            max = std::max(dp[0][0][1], dp[0][1][1]);
-        else max = dp[0][0][1];
+    if (dp[0][0][0] == dp[0][1][0]) {
+        max = std::max(dp[0][0][1], dp[0][1][1]);
+    } else if (dp[0][0][0] == min) {
+        max = dp[0][0][1];
     } else {
         max = dp[0][1][1];
     }
